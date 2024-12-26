4d46e8c257042346dee17e3e9c8d56cda0cd1d1e
Victor Tedesco <victor.augusto.tedesco@gmail.com>
2024-12-26 13:36:43 -0300

    add react-toastfy

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
 import { HomePage } from "./components/pages/HomePage";
 import { NotFoundPage } from "./components/pages/NotFoundPage";
 import { VehiclePage } from "./components/pages/VehiclePage";
+import { FavoritesPage } from "./components/pages/FavoritesPage";
+import { ToastContainer } from "react-toastify";
 
 const Layout = () => {
   return (
@@ -13,15 +15,19 @@ const Layout = () => {
 
 function App() {
   return (
+    <>
+    <ToastContainer />
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<HomePage />} />
           <Route path="vehicle" element={<VehiclePage />} />
+          <Route path="favorites" element={<FavoritesPage />} />
           <Route path="*" element={<NotFoundPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
+    </>
   );
 }
 
